a9cf5f8f5d7d75a219701c2c83149db2ef869412
Mark Wotton <mwotton@gmail.com>
2011-06-24 11:16:44 +1000

    initial loli app

---
+++ app.hs
@@ -0,0 +1,13 @@
+import Network.Loli
+import Hack.Handler.SimpleServer
+import Maybe
+import System.Environment
+
+main = do
+  port <- getEnv "PORT"
+  run (fromIntegral $ read port) . loli $ do
+
+    get "/echo/:user" $ do
+      text . ("Hi, " ++) . fromJust . lookup "user" =<< captures
+
+    get "/" (text "hello world")
